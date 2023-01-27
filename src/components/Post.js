9504d3e1902f85ffd446ba456571209aa2f016c0
Ayush Jaiswal <56079780+ayushaj-16@users.noreply.github.com>
2023-01-27 21:12:12 +0530

    add posts and posts list component

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,3 +1,5 @@
+/* ----------------------------- Post component ----------------------------- */
+
 import React, { useState } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import clsx from "clsx";
--- src/components/PostsList.js
+++ src/components/PostsList.js
@@ -1,18 +1,20 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
-import Post from './Post';
+/* -------------------------------- PostsList ------------------------------- */
+
+import React from "react";
+import { useSelector } from "react-redux";
+import Post from "./Post";
 
 export default function PostsList() {
   const posts = useSelector((state) => state.posts);
 
   const { postsList } = posts;
   const location = window.location.pathname.substr(1, 5);
-  const userId = location === 'users' ? window.location.pathname.substr(7) : '';
+  const userId = location === "users" ? window.location.pathname.substr(7) : "";
 
   return (
     <div className="posts">
       {postsList.map((post) => {
-        if (location === 'users') {
+        if (location === "users") {
           if (post.createdBy._id === userId) {
             return <Post post={post} key={post._id}></Post>;
           }
