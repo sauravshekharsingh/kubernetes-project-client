474583d0a38f8748967e14c732aa0b355ec80650
Saurav Shekhar Singh <13224960-saurav_hyperverge@users.noreply.gitlab.com>
2023-01-27 21:09:32 +0530

    setup App component

--- src/components/App.js
+++ src/components/App.js
@@ -1,3 +1,5 @@
+/* ------------------------------ App component ----------------------------- */
+
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
